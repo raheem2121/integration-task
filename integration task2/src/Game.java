a207cd15930b72fe11bdadec1874b99a85aeb3d5
Maimuna121 <74963445+Maimuna121@users.noreply.github.com>
2021-03-25 21:16:06 +0000

    Update Game.java

--- integration task2/src/Game.java
+++ integration task2/src/Game.java
@@ -205,7 +205,7 @@ public class Game {
 
     public void writeFile() {//This method reads the file containing the score
         String ScoreValue = String.valueOf(score);
-        File newFile = new File("Scores.txt");
+        File newFile = new File("https://drive.google.com/file/d/1_ITBfSrelz7h-jufZWhuSm_UcYlgHK63/view?usp=sharing");
         try {
             FileWriter write = new FileWriter(newFile, true);
             BufferedWriter buffer = new BufferedWriter(write);
